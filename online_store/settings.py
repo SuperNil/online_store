d6abca1e7cc9e162579f35e8e6fbff005b659c14
Your Name <you@example.com>
2022-09-24 18:57:14 +0500

    Second

--- online_store/settings.py
+++ online_store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j)8=^xz)(t5_9wph0*(rvm_qoo3r%ggl&ue5rr24%(b3$3cb3+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
